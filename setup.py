fb62da5e270ff0cfb265181c051533695b2c2383
Patrick Long <patrick.long@gmail.com>
2020-02-02 17:11:39 -0500

    add setup.py and update dir structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='iterativeplot',
+      version='1.0dev',
+      description='Python package for iterative plotting during exploratory data analysis',
+      author='Patrick Long',
+      packages=['iterativeplot'],
+      package_dir = {'': 'src'},
+        )
